73540a77491c890938b5379654d4f842b9d5c9e9
Wayne Botha <wayneb@webuycars.co.za>
2023-01-01 15:39:58 +0200

    Solved 25/1

--- 25/1/main.go
+++ 25/1/main.go
@@ -40,9 +40,7 @@ func EncodeSNAFU(decimal int) byte {
 	return '?'
 }
 
-func Decimal2SNAFU(decimal int) string {
-	fmt.Printf("\nConvert [%d]\n", decimal)
-
+func Decimal2SNAFU(decimal int) (s []byte) {
 	var i int
 	var p float64
 
@@ -61,14 +59,13 @@ func Decimal2SNAFU(decimal int) string {
 	for i = i - 1; i >= 0; i-- {
 		p = math.Pow(5.0, float64(i))
 		var a int = int(math.Floor(float64(v) / p))
-		fmt.Printf("(%d) %d: [%v]\n", i, int(p), a)
+		// fmt.Printf("(%d) %d: [%v]\n", i, int(p), a)
 		number[i] = a
 		v = int(math.Mod(float64(v), p))
 	}
 
 	// convert to -2 offset range
 	for x := range number {
-		fmt.Printf("%d", number[x])
 		switch number[x] {
 		case 0:
 		case 1:
@@ -85,23 +82,20 @@ func Decimal2SNAFU(decimal int) string {
 				number = append(number, 0)
 			}
 			number[x+1] += 1
+		case 5:
+			number[x] = 0
+			if x == len(number)-1 {
+				number = append(number, 0)
+			}
+			number[x+1] += 1
 		}
 	}
-	fmt.Printf("\n")
 
-	fmt.Printf("Decimal values: ")
 	for x := range number {
-		fmt.Printf("%d ", number[len(number)-x-1])
+		s = append(s, EncodeSNAFU(number[len(number)-x-1]))
 	}
-	fmt.Printf("\n")
 
-	fmt.Printf("SNAFU values: ")
-	for x := range number {
-		fmt.Printf("%c", EncodeSNAFU(number[len(number)-x-1]))
-	}
-	fmt.Printf("\n")
-
-	return ""
+	return s
 }
 
 func LoadNumbers(filePath string) {
@@ -116,66 +110,20 @@ func LoadNumbers(filePath string) {
 	var total int = 0
 	for scanner.Scan() {
 		var line string = scanner.Text()
-		fmt.Printf("%s: ", line)
+		// fmt.Printf("%s: ", line)
 
 		var decimal int = 0
 		for i, j := len(line)-1, 0; i >= 0; i, j = i-1, j+1 {
 			decimal += DecodeSNAFU(line[i]) * int(math.Pow(5, float64(j)))
-			// fmt.Printf("%d * %d\n", DecodeSNAFU(line[i]), int(math.Pow(5, float64(j))))
 		}
-		fmt.Printf("%d\n", decimal)
-		// Decimal2SNAFU(decimal)
-		// fmt.Printf("\n")
+		// fmt.Printf("%d\n", decimal)
 		total += decimal
 	}
 	fmt.Printf("TOTAL: %d\n", total)
-	// Decimal2SNAFU(total)
-
-	// fmt.Printf("\nExpecting 2=0=  2 (125) -2 (25) 0 (5) -2 (1)\n\n")
-	// Decimal2SNAFU(198)
-	fmt.Printf("\n\n\n")
-	Decimal2SNAFU(22)
-	Decimal2SNAFU(23)
-	Decimal2SNAFU(24)
-	/*
-			Decimal2SNAFU(1)
-			Decimal2SNAFU(2)
-			Decimal2SNAFU(3)
-			Decimal2SNAFU(4)
-			Decimal2SNAFU(5)
-			Decimal2SNAFU(6)
-			Decimal2SNAFU(7)
-			Decimal2SNAFU(8)
-			Decimal2SNAFU(9)
-			Decimal2SNAFU(10)
-			Decimal2SNAFU(15)
-			Decimal2SNAFU(20)
-			Decimal2SNAFU(2022)
-			Decimal2SNAFU(12345)
-			Decimal2SNAFU(314159265)
-
-			Decimal2SNAFU(1747)
-			Decimal2SNAFU(906)
-			Decimal2SNAFU(198)
-			Decimal2SNAFU(11)
-
-			1=-0-2     1747
-		 12111      906
-		  2=0=      198
-		    21       11
-		  2=01      201
-		   111       31
-		 20012     1257
-		   112       32
-		 1=-1=      353
-		  1-12      107
-		    12        7
-		    1=        3
-		   122       37
-	*/
+	fmt.Printf("%s\n", Decimal2SNAFU(total))
 }
 
 func main() {
-	LoadNumbers("../sample.txt")
-	// LoadNumbers("../puzzle.txt")
+	// LoadNumbers("../sample.txt")
+	LoadNumbers("../puzzle.txt")
 }
